9247a3441091c12e68d76ec319241a1a84d1ac3c
Julian Gruber <julian@juliangruber.com>
2013-10-08 11:01:33 +0100

    016: use libs

--- 016-client.js
+++ 016-client.js
@@ -8,6 +8,8 @@ var tee = require('tee-1');
 var MuxDemux = require('mux-demux');
 var webaudio = require('webaudio');
 var through = require('through');
+var bufToAudioStream = require('./lib/buf-to-audio');
+var webaudioStream = require('./lib/webaudio-stream');
 
 var db = levelup('db', { db: leveljs });
 db.on('open', function() { db.db.idb.clear() });
@@ -19,33 +21,7 @@ window.fs = fs;
 var mdm = MuxDemux();
 
 mdm.createReadStream('random')
-.pipe(through(function(buf) {
-  this.queue(buf
-  .split('')
-  .map(function(c) {
-    var n = (255/2 - c.charCodeAt(0))/255;
-    n = Math.min(n, 1);
-    n = Math.max(n, -1);
-    return n;
-  }));
-}))
+.pipe(bufToAudioStream())
 .pipe(webaudioStream());
 
 mdm.pipe(shoe('/shoe')).pipe(mdm);
-
-function webaudioStream() {
-  var buf = [];
-
-  var channel = webaudio(function(time) {
-    if (!buf.length) return Math.random();
-    return buf.shift();
-  });
-  channel.play();
-  return through(write);
-
-  function write(chunk) {
-    buf = buf.concat(chunk);
-    if (buf.length > 1000) return false;
-  }
-}
-
